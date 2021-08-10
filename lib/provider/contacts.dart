440f61c10f40e075a84cd528973dfc142ad1c28c
Júlia Santos <juliasantos.js31405@gmail.com>
2021-08-10 20:07:01 -0300

    commit inicial

---
+++ lib/provider/contacts.dart
@@ -0,0 +1,55 @@
+import 'package:agenda_de_contatos/models/contact.dart';
+
+List<Contact> contacts = [
+  Contact(
+    name: "Nicole Santiago",
+    photo: "assets/images/Nicole.jpg",
+    phone: "+55 51 11111-1111",
+    email: "nicole.santiago@gmail.com",
+  ),
+  Contact(
+    name: "Camila Martins",
+    photo: "assets/images/Camila.jpg",
+    phone: "+55 51 22222-2222",
+    email: "camila.martins@gmail.com",
+    isFavorite: true,
+  ),
+  Contact(
+    name: "Paulo Rodrigues",
+    photo: "assets/images/Paulo.jpg",
+    phone: "+55 51 33333-3333",
+    email: "paulo.rodrigues@gmail.com",
+  ),
+  Contact(
+    name: "Lucas Lopes",
+    photo: "assets/images/Lucas.jpg",
+    phone: "+55 51 44444-4444",
+    email: "lucas.lopes@gmail.com",
+    isFavorite: true,
+  ),
+  Contact(
+    name: "Juliana Fernandes",
+    photo: "assets/images/Juliana.jpg",
+    phone: "+55 51 55555-555",
+    email: "juliana.fernandes@gmail.com",
+  ),
+  Contact(
+    name: "Thomas Astrucs",
+    photo: "assets/images/Thomas.jpg",
+    phone: "+55 51 66666-6666",
+    email: "thomas.astrucs@gmail.com",
+    isFavorite: true,
+  ),
+  Contact(
+    name: "Claudia de Paula",
+    photo: "assets/images/Claudia.jpg",
+    phone: "+55 51 77777-7777",
+    email: "claudia.de.paula@gmail.com",
+  ),
+  Contact(
+    name: "Marina Stallin",
+    photo: "assets/images/Marina.jpg",
+    phone: "+55 51 88888-8888",
+    email: "nicole.stallin@gmail.com",
+  ),
+];
